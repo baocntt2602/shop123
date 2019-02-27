4f0b091b211728b3315c84e877db7389cff92e6f
DuyBao <bao.nguyen@axonactive.com>
2019-02-27 20:36:36 +0700

    first commit

---
+++ src/main/java/vn/educom/leading/shop123/Shop123Application.java
@@ -0,0 +1,13 @@
+package vn.educom.leading.shop123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop123Application.class, args);
+	}
+
+}
